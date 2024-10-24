b7e8ec798f246ce38145994b81bb1cb932dec361
AhnafMubashshir <bsse1232@iit.du.ac.bd>
2024-10-24 20:25:55 +0600

    url fixed

--- registration/registration/urls.py
+++ registration/registration/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('user/', include('registration_management.urls')),
-    path('healthcheck',HealthCheckView.as_view(),name='health check')
+    path('v1/registration/healthcheck',HealthCheckView.as_view(),name='health check')
 ]

a324e2ea16f0901e5fd7ee0bdd6a79bb270d5145
AhnafMubashshir <bsse1232@iit.du.ac.bd>
2024-10-25 09:03:59 +0600

    ffddf

--- ticket/ticket_management/redis_utils.py
+++ ticket/ticket_management/redis_utils.py
@@ -5,5 +5,5 @@ from django.core.cache import cache
 from django_redis import get_redis_connection
 # Connect to Redis
 redis_client = get_redis_connection("default")
-redis_client.config_set("notify-keyspace-events", "Ex")
+# redis_client.config_set("notify-keyspace-events", "Ex")
 
